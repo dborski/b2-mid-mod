904523455e4a9c9b59e91c39bcd39e4078845d5f
Derek Borski <derek.borski@gmail.com>
2020-05-22 10:03:06 -0600

    Add studios index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+studio1 = Studio.create(name: "studio1", location: "location1")
+studio2 = Studio.create(name: "studio2", location: "location2")
+
+movie1 = Movie.create(title: "movie1", creation_year: "year1", genre: "genre1", studio_id: "#{studio1.id}")
+movie2 = Movie.create(title: "movie2", creation_year: "year2", genre: "genre2", studio_id: "#{studio1.id}")
+movie3 = Movie.create(title: "movie3", creation_year: "year3", genre: "genre3", studio_id: "#{studio2.id}")
--- spec/features/studios/studio_index_spec.rb
+++ spec/features/studios/studio_index_spec.rb
@@ -4,26 +4,26 @@ describe "user sees all studios" do
   describe "they visit /studios" do
     it "displays all studios" do
 
-      studio1 = Studio.new(name: "studio1", location: "location1")
-      studio2 = Studio.new(name: "studio2", location: "location2")
+      studio1 = Studio.create!(name: "studio1", location: "location1")
+      studio2 = Studio.create!(name: "studio2", location: "location2")
 
-      movie1 = Movie.new(title: "movie1", creation_year: "year1", genre: "genre1", studio_id: "#{studio1.id}")
-      movie2 = Movie.new(title: "movie2", creation_year: "year2", genre: "genre2", studio_id: "#{studio1.id}")
-      movie3 = Movie.new(title: "movie3", creation_year: "year3", genre: "genre3", studio_id: "#{studio2.id}")
+      movie1 = Movie.create!(title: "movie1", creation_year: "year1", genre: "genre1", studio_id: "#{studio1.id}")
+      movie2 = Movie.create!(title: "movie2", creation_year: "year2", genre: "genre2", studio_id: "#{studio1.id}")
+      movie3 = Movie.create!(title: "movie3", creation_year: "year3", genre: "genre3", studio_id: "#{studio2.id}")
 
-      visit "/studios"
+      visit '/studios'
 
-      within("#studio-#{studio1.id}") do
+      within "#studio-#{studio1.id}" do
         expect(page).to have_content(studio1.name)
         expect(page).to have_content(studio1.location)
-        expect(page).to have_content(movie1.name)
-        expect(page).to have_content(movie2.name)
+        expect(page).to have_content(movie1.title)
+        expect(page).to have_content(movie2.title)
       end
 
-      within("#studio-#{studio2.id}") do
+      within "#studio-#{studio2.id}" do
         expect(page).to have_content(studio2.name)
         expect(page).to have_content(studio2.location)
-        expect(page).to have_content(movie3.name)
+        expect(page).to have_content(movie3.title)
       end
     end
   end
